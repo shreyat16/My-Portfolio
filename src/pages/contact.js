7b0b434470ddf73ea0d27709681abba077e23372
Shreya <sthamm2001@gmail.com>
2020-06-28 20:12:07 -0400

    minor changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import {Link} from 'gatsby'
 import mailtoLink from 'mailto-link'
+import "./contact.css"
 
 const ContactPage = () => {
   return(
@@ -12,7 +13,7 @@ const ContactPage = () => {
         <h2><a href="https://www.instagram.com/shreya_thammana/">Link to my Instagram</a></h2>
         <h2><a href="https://www.youtube.com/channel/UCV928BgjhuiNglyFRSYQYuw?pbjreload=101">Link to my YouTube</a></h2>
         <h2><a href="https://github.com/shreyat16">Link to my GitHub</a></h2>
-        <h2><a href={mailtoLink({to: 'shreyat@clemson.edu'})}>Send me an email</a></h2>
+        <h2 className="emailname"><a href={mailtoLink({to: 'shreyat@clemson.edu'})}>Send me an email</a></h2>
       </div>
       
     </Layout>
